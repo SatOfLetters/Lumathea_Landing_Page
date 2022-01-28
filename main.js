4b420678cdc53beee02c0ca3a71b824fe3953eb9
SatOfLetters <97686395+SatOfLetters@users.noreply.github.com>
2022-01-27 23:34:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const hambt = document.querySelector('.hambt')
 const nav = document.querySelector('header nav')
 
+const hamDivs = document.querySelectorAll('.hambt div')
+
 let showingNav = false;
 
 function toggleNav() {
     if(showingNav == false) {
         showingNav = true;
         nav.classList.add('showing_nav')
+
+        for(i=0;i<hamDivs.length;i++) {
+            hamDivs[i].style.backgroundColor = 'var(--lighta)';
+        }
     } else {
         showingNav = false;
         nav.classList.remove('showing_nav')
+
+        for(i=0;i<hamDivs.length;i++) {
+            hamDivs[i].style.backgroundColor = 'var(--c3)';
+        }
     }
 }
 
